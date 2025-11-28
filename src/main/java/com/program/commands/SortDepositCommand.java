5b59978eb57c58d4ff526f03d762566dc686adaa
Volodymyr <sss@>
2025-11-28 23:31:43 +0200

    sort

--- src/main/java/com/program/commands/SortDepositCommand.java
+++ src/main/java/com/program/commands/SortDepositCommand.java
@@ -1,13 +1,27 @@
 package com.program.commands;
 
+import com.program.manager.DepositManager;
+
 public class SortDepositCommand implements Command{
+
+    private final DepositManager depositManager;
+
+    public SortDepositCommand(DepositManager depositManager) {
+        this.depositManager = depositManager;
+    }
+
+
     @Override
     public String getDesc() {
-        return "";
+        return "Сортує депозити всіх банків за командами (id, owner, duration, rate, bank)";
     }
 
     @Override
     public void execute(String param) {
-
+        if (param.isEmpty()) {
+            System.out.println("Метод сортування не заданий!!!");
+        } else {
+            depositManager.sort(param);
+        }
     }
 }
--- src/main/java/com/program/manager/DepositManager.java
+++ src/main/java/com/program/manager/DepositManager.java
@@ -5,6 +5,7 @@ import com.program.bank.Deposit;
 import lombok.Getter;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Random;
 
@@ -19,9 +20,18 @@ public class DepositManager {
         loadDeposits();
     }
 
-    public void sortByAmmount(){
-
+    public void sort(String field) {
+        switch (field.toLowerCase()) {
+            case "id"       -> deposits.sort(Comparator.comparingInt(Deposit::getDepositId));
+            case "owner"    -> deposits.sort(Comparator.comparing(Deposit::getOwnerName));
+            case "bank"     -> deposits.sort(Comparator.comparing(Deposit::getBankName));
+            case "amount"   -> deposits.sort(Comparator.comparingDouble(Deposit::getAmount));
+            case "rate"     -> deposits.sort(Comparator.comparingDouble(Deposit::getInterestRate));
+            case "duration" -> deposits.sort(Comparator.comparingInt(Deposit::getDurationMonths));
+            default -> System.out.println("Невідомий параметр сортування!");
+        }
     }
+
 
     public void addDeposit(Deposit deposit){
         deposits.add(deposit);
